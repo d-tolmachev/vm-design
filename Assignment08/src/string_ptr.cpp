b0ea258dc9b32048e4c9366994092726fee4d7c8
d-tolmachev <den.filipchenko0000@gmail.com>
2026-05-23 01:55:56 +0300

    [Assignment08] Inline functions and `allocate` instead of `strdup`

--- Assignment08/src/string_ptr.cpp
+++ Assignment08/src/string_ptr.cpp
@@ -1,22 +1,9 @@
 #include "string_ptr.h"
 
-#include <cstring>
 #include <memory>
 #include <utility>
 
 namespace assignment_08 {
-
-    string_ptr::string_ptr() noexcept
-        : repr_(to_repr(nullptr, false)) {
-    }
-
-    string_ptr::string_ptr(const char* str) noexcept
-        : repr_(to_repr(str ? strdup(str) : nullptr, str != nullptr)) {
-    }
-
-    string_ptr::string_ptr(std::string_view str) noexcept
-        : string_ptr(str.data()) {
-    }
 
     string_ptr::string_ptr(const string_ptr& other) noexcept
         : repr_(other.repr_ & PTR) {
@@ -43,29 +30,9 @@ namespace assignment_08 {
         return *this;
     }
 
-    bool string_ptr::unique() const noexcept {
-        return static_cast<bool>(repr_ & UNIQUE);
-    }
-
-    char* string_ptr::data() const noexcept {
-        return reinterpret_cast<char*>(repr_ & PTR);
-    }
-
     string_ptr::operator std::string_view() const noexcept {
         char* ptr = data();
         return std::string_view{ptr ? ptr : ""};
-    }
-
-    void string_ptr::swap(string_ptr& other) noexcept {
-        std::swap(repr_, other.repr_);
-    }
-
-    uintptr_t string_ptr::to_repr(const char* ptr, bool unique) noexcept {
-        return (reinterpret_cast<uintptr_t>(ptr) | unique);
-    }
-
-    std::ostream& operator<<(std::ostream& os, const string_ptr& str) {
-        return os << (str.unique() ? "unique" : "non-unique") << " string: \"" << static_cast<std::string_view>(str) << '\"';
     }
 
 }
--- Assignment08/src/string_ptr.h
+++ Assignment08/src/string_ptr.h
@@ -1,8 +1,10 @@
 #ifndef STRING_PTR_H
 #define STRING_PTR_H
 
+#include <algorithm>
 #include <cstdint>
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <ostream>
 #include <string_view>
@@ -13,9 +15,9 @@ namespace assignment_08 {
     public:
         string_ptr() noexcept;
 
-        string_ptr(const char* str) noexcept;
+        string_ptr(const char* str);
 
-        string_ptr(std::string_view str) noexcept;
+        string_ptr(std::string_view str);
 
         string_ptr(const string_ptr& other) noexcept;
 
@@ -27,13 +29,13 @@ namespace assignment_08 {
 
         ~string_ptr();
 
-        [[nodiscard]] bool unique() const noexcept;
+        [[nodiscard]] constexpr bool unique() const noexcept;
 
         [[nodiscard]] char* data() const noexcept;
 
         operator std::string_view() const noexcept;
 
-        void swap(string_ptr& other) noexcept;
+        constexpr void swap(string_ptr& other) noexcept;
 
     private:
         constexpr static uintptr_t UNIQUE = 1;
@@ -41,9 +43,21 @@ namespace assignment_08 {
         mutable uintptr_t repr_;
 
         static uintptr_t to_repr(const char* ptr, bool unique) noexcept;
+
+        static char* allocate(const char* str);
     };
 
-    std::ostream& operator<<(std::ostream& os, const string_ptr& str);
+    inline string_ptr::string_ptr() noexcept
+        : repr_(to_repr(nullptr, false)) {
+    }
+
+    inline string_ptr::string_ptr(const char* str)
+        : repr_(to_repr(str ? allocate(str) : nullptr, str != nullptr)) {
+    }
+
+    inline string_ptr::string_ptr(std::string_view str)
+        : string_ptr(str.data()) {
+    }
 
     inline string_ptr::~string_ptr() {
         if (unique()) {
@@ -52,6 +66,33 @@ namespace assignment_08 {
 #endif
             std::free(data());
         }
+    }
+
+    constexpr inline bool string_ptr::unique() const noexcept {
+        return static_cast<bool>(repr_ & UNIQUE);
+    }
+
+    inline char* string_ptr::data() const noexcept {
+        return reinterpret_cast<char*>(repr_ & PTR);
+    }
+
+    constexpr inline void string_ptr::swap(string_ptr& other) noexcept {
+        std::swap(repr_, other.repr_);
+    }
+
+    inline uintptr_t string_ptr::to_repr(const char* ptr, bool unique) noexcept {
+        return (reinterpret_cast<uintptr_t>(ptr) | unique);
+    }
+
+    inline char* string_ptr::allocate(const char* str) {
+        size_t length = std::strlen(str);
+        char* ptr = new char[length + 1];
+        std::copy(str, str + length + 1, ptr);
+        return ptr;
+    }
+
+    inline std::ostream& operator<<(std::ostream& os, const string_ptr& str) {
+        return os << (str.unique() ? "unique" : "non-unique") << " string: \"" << static_cast<std::string_view>(str) << '\"';
     }
 
 }
